cb50b6f701a667970c1bf7f148717b36d2c71165
maldo <maldo@LAPTOP-PPPO3N3Q>
2023-06-08 08:16:54 -0600

    Agregar archivos de proyecto.

---
+++ Vehiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen_Final
+{
+    public class Vehiculo
+    {
+        string numeroP;
+        string marca;
+        string modelo;
+    }
+}
